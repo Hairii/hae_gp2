27bb02cba4dce75af5f2d5ac3bd56b9ed795b334
Léila <dugarcein.leila16@gmail.com>
2020-09-24 13:59:12 +0200

    probleme git

---
+++ Cour 0/ConsoleApplication1/ConsoleApplication1/Vec2.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class Vec2 {
+public:
+	Vec2(float x = 0.f,float y = 0.0f) {
+		this->x = x;
+		this->y = y;
+	}
+
+	~Vec2() {
+
+	}
+
+	Vec2 add(Vec2 v) {
+		Vec2 xy;
+
+		xy = this->
+	}
+};
